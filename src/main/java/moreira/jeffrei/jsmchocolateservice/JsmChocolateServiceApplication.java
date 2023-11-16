4af077ff49d134ca168f73e5316efdf9e806ee4f
Jeffrei S. Moreira <jeffreism@yahoo.com>
2023-11-15 22:34:52 -0500

    Initial commit. Closes #1

---
+++ src/main/java/moreira/jeffrei/jsmchocolateservice/JsmChocolateServiceApplication.java
@@ -0,0 +1,13 @@
+package moreira.jeffrei.jsmchocolateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsmChocolateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsmChocolateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moreira/jeffrei/jsmchocolateservice/JsmChocolateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package moreira.jeffrei.jsmchocolateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsmChocolateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
