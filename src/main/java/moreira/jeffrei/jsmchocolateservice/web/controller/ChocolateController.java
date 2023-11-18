a452b3926977e6a5c00e6f6321635b7ea5ac455a
Jeffrei S. Moreira <jeffreism@yahoo.com>
2023-11-17 21:26:47 -0500

    Adding Chocolate entity. Closes #5

--- src/main/java/moreira/jeffrei/jsmchocolateservice/web/controller/ChocolateController.java
+++ src/main/java/moreira/jeffrei/jsmchocolateservice/web/controller/ChocolateController.java
@@ -1,6 +1,7 @@
 package moreira.jeffrei.jsmchocolateservice.web.controller;
 
 import moreira.jeffrei.jsmchocolateservice.web.model.ChocolateDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,11 @@ public class ChocolateController {
     @PostMapping
     public ResponseEntity saveNewChocolate(@RequestBody ChocolateDto dto) {
         // todo impl
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        // TODO add hostname to url
+        headers.add("Location", "/api/v1/chocolate/" + UUID.randomUUID().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+        //return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{chocolateId}")
@@ -32,5 +37,12 @@ public class ChocolateController {
 
         // todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteChocolate(@PathVariable UUID id) {
+        // todo impl
     }
 }
